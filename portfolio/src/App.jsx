cfe129ed85dd9b39bd7d81cb92c18396a8096a34
GG-Gx <78389994+GG-Gx@users.noreply.github.com>
2024-05-24 15:41:17 +0200

    net

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,19 +5,6 @@ import Experience from './components/Experience'
 import Projects from './components/Projects'
 import About from './components/About'
 import Footer from './components/Footer'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  Outlet,
-  useNavigate,
-  Routes,
-  Router,
-  BrowserRouter,
-
-} from "react-router-dom";
-
 import Navbarsmall from './components/Navbarsmall'
 
 import './App.css'
