11b75b7a13104d6daead12e101416415e21f4a79
GG-Gx <78389994+GG-Gx@users.noreply.github.com>
2024-12-05 11:12:11 -0300

    load

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Experience from './components/Experience'
-import Projects from './components/Projects'
-import About from './components/About'
-import Footer from './components/Footer'
-import Navbarsmall from './components/Navbarsmall'
+import { useState, useEffect } from 'react';
+import { Spinner, Center, Box } from '@chakra-ui/react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import About from './components/About';
+import Footer from './components/Footer';
+import Navbarsmall from './components/Navbarsmall';
 
-import './App.css'
-
+import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+
+    const timer = setTimeout(() => setLoading(false), 1000); // 
+    return () => clearTimeout(timer); 
+  }, []);
+
+  if (loading) {
+    return (
+      <Center h="100vh">
+        <Spinner size="xl" color="red.500" thickness="4px" speed="0.65s"
+         />
+      </Center>
+    );
+  }
 
   return (
     <>
-    <Navbar />
-    <Navbarsmall />
-    <Header />
-    <Experience />
-    <Projects />
-    <About />
-    <Footer />
-    
-
-
+      <Navbar />
+      <Navbarsmall />
+      <Header />
+      <Experience />
+      <Projects />
+      <About />
+      <Footer />
     </>
-  
-  )
+  );
 }
 
-export default App
+export default App;
